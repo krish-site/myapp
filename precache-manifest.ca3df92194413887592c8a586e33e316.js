06fba27a5d71316ee4a08826e21181160eeb3f20
krish-site <17ics042@gbu.ac.in>
2020-01-23 06:25:10 +0530

    Updates

---
+++ precache-manifest.ca3df92194413887592c8a586e33e316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda83192172b21e832cb18e4a4aac98d",
+    "url": "/krish-site/myapp/index.html"
+  },
+  {
+    "revision": "6221c89fa155961c6e0e",
+    "url": "/krish-site/myapp/static/js/2.d6406216.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/krish-site/myapp/static/js/2.d6406216.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96faf4fb4573f8dd338d",
+    "url": "/krish-site/myapp/static/js/main.f64e11c0.chunk.js"
+  },
+  {
+    "revision": "10eb037d16c6bd2c1a5e",
+    "url": "/krish-site/myapp/static/js/runtime-main.df51a163.js"
+  }
+]);
